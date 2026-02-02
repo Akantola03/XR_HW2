55d51f99cf4360be70c994df0c7246e01c675263
Atte <atte.kantola@student.oulu.fi>
2026-02-02 15:31:55 +0200

    Mangnifyinglass update
    Mangnifyinglass works somewhat correctly. Made so that the text on the other wall can only be seen through the lens.

---
+++ Assets/LensVector.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LensVector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Vector3 rotation = new Vector3(0.0f, 0.75f, 0.0f);
+        //transform.Rotate(rotation);
+
+        transform.position = new Vector3(0.0f, 3.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
